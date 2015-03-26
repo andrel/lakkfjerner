db49038bcaf211e801a4907820feed406a1d6eb3
andrel <andre@lindhjem.net>
2015-03-26 21:20:09 +0100

    Refactor javascript.

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
-(function() {
-var cookieApi = chrome.cookies;
-var tabsApi = chrome.tabs;
+(function(chrome, _) {
+  var cookieApi = chrome.cookies;
+  var runtimeApi = chrome.runtime;
 
-cookieApi.onChanged.addListener(filterCookies);
-chrome.runtime.onStartup.addListener(removeCookiesOnce);
+  // Register listeners.
+  cookieApi.onChanged.addListener(filterCookies);
+  runtimeApi.onStartup.addListener(removeCookiesOnce);
 
+  function filterCookies(changeinfo) {
+    var cookie = changeinfo.cookie;
+    var cause = changeinfo.cause;
+    var cookie_removed = changeinfo.removed;
 
-function filterCookies(changeinfo) {
-  var cookie = changeinfo.cookie;
-  var cause = changeinfo.cause;
-  var cookie_removed = changeinfo.removed;
-
-  if (cause === 'evicted' || cause === 'expired') {
-    return;
+    if (cause === 'evicted' || cause === 'expired') {
+      return;
+    } else {
+      attemptRemove(cookie);
+    }
   }
 
-  attemptRemove(cookie);
-}
-
-function removeCookiesOnce() {
-  cookieApi.getAll({domain: 'aftenposten.no'}, function(cookies) {
-    forEach(cookies, attemptRemove);
-  });
-}
-
-function attemptRemove(cookie) {
-  if (/^VPW_Quota*/.test(cookie.name)) {
-
-    var toRemove = {
-      url: "http" + ((cookie.secure) ? 's' : '') + '://' + cookie.domain + cookie.path,
-      name: cookie.name
-  	};
-    cookieApi.remove(toRemove, function(details) {
-      console.assert(details != null, 'Could not remove cookie');
-      //console.log('Blokkert kjeks! (' + details.storeId + ' / ' + details.name + ' / ' + details.url + ')');
+  function removeCookiesOnce() {
+    cookieApi.getAll({domain: 'aftenposten.no'}, function(cookies) {
+      _.forEach(cookies, attemptRemove);
     });
   }
-}
 
-function forEach(array, action) {
-  for (var i = 0; i < array.length; i++) {
-    action(array[i]);
+  function attemptRemove(cookie) {
+    if (/^VPW_Quota*/.test(cookie.name)) {
+      var toRemove = {
+        url: "http" + ((cookie.secure) ? 's' : '') + '://' + cookie.domain + cookie.path,
+        name: cookie.name
+    	};
+      cookieApi.remove(toRemove, function(details) {
+      console.assert(details != null, 'Could not remove cookie');
+      console.log('Blokkert kjeks! (' + details.storeId + ' / ' + details.name + ' / ' + details.url + ')');
+      });
+    }
   }
-}
-})()
+})(chrome, Utils)
+
+var Utils = (function() {
+
+  var forEach = function(array, action) {
+    for (var i = 0; i < array.length; i++) {
+      action(array[i]);
+    }
+  }
+
+  return {
+    forEach: forEach
+  }
+})();
