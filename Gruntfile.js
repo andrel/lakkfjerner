2b86ea8ae8a31a20448d2a600e311d6cb6a4504f
andlin <andre@lindhjem.net>
2015-04-11 22:29:23 +0200

    Grunt clean plugin
    - Deletes generate build-folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,12 +22,14 @@ module.exports = function(grunt) {
 	    //files: ['<%= jshint.files %>'],
 	    files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
 	    tasks: ['jshint']
-	}
+	},
+    clean: ['build']
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
     grunt.registerTask('default', ['jshint', 'uglify']);
 };
