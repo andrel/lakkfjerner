4d4e284cf8d111f47210f1c72553a22e4eac2851
andlin <andre@lindhjem.net>
2015-04-11 22:29:40 +0200

    Reformat code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,29 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration
     grunt.initConfig({
-	pkg: grunt.file.readJSON('package.json'),
-	uglify: {
-	    options: {
-		banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-	    },
-	    build: {
-		files: {
-		    'build/main.min.js': ['src/main.js', 'src/app.js']
-		}
-	    }
-	},
-	jshint: {
-	    files: {
-		src: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
-	    }
-	},
-	watch: {
-	    //files: ['<%= jshint.files %>'],
-	    files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
-	    tasks: ['jshint']
-	},
-    clean: ['build']
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                files: {
+                    'build/main.min.js': ['src/main.js', 'src/app.js']
+                }
+            }
+        },
+        jshint: {
+            files: {
+                src: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+            }
+        },
+        watch: {
+            //files: ['<%= jshint.files %>'],
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            tasks: ['jshint']
+        },
+        clean: ['build']
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
